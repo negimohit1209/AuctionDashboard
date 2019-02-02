4bda0a304d6ec85922f82ceacfb3232ba0153362
Mohit Negi <mohitsngh1997@gmail.com>
2019-02-02 15:20:16 +0530

    add paper and a few corrections

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React, { Component } from 'react';
 import PlayerDashboard from './containers/PlayerDashboard/PlayerDashboard';
 import TeamDashboard from './containers/TeamDashboard/TeamDashboard';
 import './App.css';
+import Paper from './components/Paper/Paper';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className = "child">
-          <PlayerDashboard/>
+        <Paper><PlayerDashboard/></Paper>
+          
         </div>
         <div className = "child">
+        <Paper>
           <TeamDashboard />
+        </Paper>
+         
         </div>
       </div>
     );
